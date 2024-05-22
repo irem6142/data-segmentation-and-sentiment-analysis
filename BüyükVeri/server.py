bd81bec0b544a0aadd8a5867e96f3e19e6aee2e4
rumeysa <rmysclk61@icloud.com>
2024-05-22 21:37:51 +0300

    first commit

--- BüyükVeri/server.py
+++ BüyükVeri/server.py
@@ -1,5 +1,6 @@
+
 from flask import Flask, request, jsonify
-from flask_cors import CORS  # Flask-CORS kütüphanesini import et
+from flask_cors import CORS
 from bs4 import BeautifulSoup
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from mtranslate import translate
@@ -7,8 +8,9 @@ import requests
 
 import nltk
 nltk.download('vader_lexicon')
+
 app = Flask(__name__)
-CORS(app)  # Flask uygulamasına CORS desteğini ekleyin
+CORS(app)
 
 class WebScraper:
     def __init__(self):
@@ -28,15 +30,22 @@ class WebScraper:
 def analyze_sentiment(comments):
     sid = SentimentIntensityAnalyzer()
     analyzed_complaints = []
+    positive_count = 0
+    negative_count = 0
     for comment in comments:
         scores = sid.polarity_scores(comment)
         if scores['compound'] > 0.05:
             analyzed_complaints.append((comment, "Positive"))
+            positive_count += 1
         elif scores['compound'] < -0.05:
             analyzed_complaints.append((comment, "Negative"))
+            negative_count += 1
         else:
             analyzed_complaints.append((comment, "Neutral"))
-    return analyzed_complaints
+    total_comments = len(comments)
+    positive_ratio = positive_count / total_comments if total_comments > 0 else 0
+    negative_ratio = negative_count / total_comments if total_comments > 0 else 0
+    return analyzed_complaints, positive_ratio, negative_ratio
 
 def translate_to_english(texts):
     translations = []
@@ -52,7 +61,7 @@ def process_url():
     scraper = WebScraper()
     comments = scraper.scrape_comment(url)
     translations = translate_to_english(comments)
-    sentiments = analyze_sentiment(translations)
+    sentiments, positive_ratio, negative_ratio = analyze_sentiment(translations)
 
     # Çıktıyı oluştur
     output = ""
@@ -60,8 +69,12 @@ def process_url():
         output += "Şikayet: " + complaint + "\n"
         output += "Duygu Durumu: " + sentiment + "\n"
         output += "--------------\n"
+    
+    output += f"Pozitif Duygu Oranı: {positive_ratio:.2f}\n"
+    output += f"Negatif Duygu Oranı: {negative_ratio:.2f}\n"
 
     return jsonify({"output": output})
 
 if __name__ == '__main__':
     app.run(debug=True)
+
